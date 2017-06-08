b0259fb136eff0ccd73dec57f13c096fa7dcc2df
passion1127 <672301997@qq.com>
2017-06-08 19:49:14 +0800

    as

--- src/main/java/com/daniu/demo/Demo01.java
+++ src/main/java/com/daniu/demo/Demo01.java
@@ -1,7 +1,32 @@
 package com.daniu.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Demo01 {
+	
+	private String name ;
+	
+	/*public Demo01(OutputStream out) {
+		this.out = out;
+	}*/
+	
+	
 	public static void main(String[] args) {
-		System.out.println("test maven plugin........");
+		System.out.println("test spring ........");
+		
+		ApplicationContext application = new ClassPathXmlApplicationContext("classpath:application.xml"); 
+		Demo01 demo = (Demo01) application.getBean("demo01");
+		System.out.println("name : " + demo.getName());
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
 }
---
+++ src/main/java/com/daniu/soundsystem/CompactDisc.java
@@ -0,0 +1,7 @@
+package com.daniu.soundsystem;
+
+public interface CompactDisc {
+	
+	void play();
+	
+}
